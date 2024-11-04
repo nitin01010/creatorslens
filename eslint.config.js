50fdd69ee53083c97aa0a9fae2940a35c1bbe0ec
Nitin0202 <nitinsachdeva0202@gmail.com>
2024-11-05 00:15:03 +0530

    get all youtube videos

--- eslint.config.js
+++ eslint.config.js
@@ -1,38 +1,46 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
 
 export default [
-  { ignores: ['dist'] },
+  {
+    ignores: ['dist'], // Ignore 'dist' folder
+  },
   {
     files: ['**/*.{js,jsx}'],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: globals.browser, // Using globals for browser environment
       parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
+        ecmaVersion: 'latest', // Use the latest ECMAScript features
+        ecmaFeatures: {
+          jsx: true, // Enable JSX
+        },
+        sourceType: 'module', // Allow using ES modules
       },
     },
-    settings: { react: { version: '18.3' } },
+    settings: {
+      react: {
+        version: 'detect', // Automatically detect the version of React
+      },
+    },
     plugins: {
-      react,
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      react, // React plugin for linting
+      'react-hooks': reactHooks, // React Hooks plugin
+      'react-refresh': reactRefresh, // React Refresh plugin
     },
     rules: {
       ...js.configs.recommended.rules,
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
-      'react/jsx-no-target-blank': 'off',
+      'react/jsx-no-target-blank': 'off', // Allow target="_blank" without rel
       'react-refresh/only-export-components': [
         'warn',
-        { allowConstantExport: true },
+        { allowConstantExport: false }, // Allow exporting constant components
       ],
     },
   },
-]
+];
--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,17 @@ import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import { Auth0Provider } from '@auth0/auth0-react';
 
+const domain = import.meta.env.VITE_AUTH0_DOMAIN;
+const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <Auth0Provider
-            domain="dev-xrum4bpzggdoilx1.us.auth0.com"
-            clientId="kbQDo3GsyJNcZAdXgS8ZYwtudm6qxxlX"
+            domain={ domain }
+            clientId={ clientId }
             authorizationParams={ {
-                redirect_uri: window.location.origin
+                redirect_uri: window.location.origin,
             } }
         >
             <BrowserRouter>
